1156fe1e680a25ec7d4b966c23638749c6670202
Rishabh Kumar <rishabh845401@gmail.com>
2023-05-23 21:06:26 +0530

    Adding files in the project

---
+++ app/src/main/java/com/example/tic_tac_toe/StartGameActivity.kt
@@ -0,0 +1,21 @@
+package com.example.tic_tac_toe
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_start_game.*
+
+class StartGameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start_game)
+
+
+
+        button2.setOnClickListener {
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
